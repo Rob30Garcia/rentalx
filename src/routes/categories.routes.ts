4676e574f9554b191da1166318d6ce37829f3303
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-01-16 21:41:40 -0400

    chore: use methods of the repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,23 @@
 import { Router } from 'express';
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  categoriesRepository.create({ name, description });
 
-  Object.assign(category, {
-    name, 
-    description,
-    created_at: new Date()
-  });
+  return response.status(201).send();
 
-  categories.push(category);
+});
 
-  return response.status(201).json(category);
+categoriesRoutes.get('/', (request, response) => {
+  const categories = categoriesRepository.list();
 
-}); 
+  return response.status(200).json(categories);
+});
 
 export { categoriesRoutes };
